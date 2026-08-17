329af233442c636d9a7405126b7686481cbb5115
GISA Dan <d.gisa2@alustudent.com>
2026-08-17 15:09:18 +0200

    added bcryptjs to hash users' passwords & functions to compare hashed password to new entered one if they match

--- backend/modules/user.js
+++ backend/modules/user.js
@@ -15,4 +15,15 @@ const userschema = new mongoose.schema (
     { timestamps: true }
 );
 
-userschema.pre('save', async function (next) {})
+userschema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userschema.methods.matchPassword = function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userschema);
